273cb7e0bc1a7fb15516551e688fb75436a80316
froesalexandre94-DC <froesalexandre94@gmail.com>
2025-11-09 22:03:31 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,5 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
 };
-<<<<<<< HEAD
+
 export default nextConfig;
-=======
-export default nextConfig;
->>>>>>> e9747fdfff5556c95edbba147da4e56b70c17c13
